dca18ead0539d4747d014e8e1bbfe063272af9c1
Eloka Ogbonnia <elokakene@gmail.com>
2022-12-16 23:22:21 +0100

    Update Section.js

--- src/Section.js
+++ src/Section.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import BookCard from './BookCard',
+import BookCard from './BookCard';
 
 
 
@@ -11,23 +11,49 @@ class Section extends Component {
     }
 
     bookshelfing = () => {
-
+        
     }
 
     render() {
         const {currentlyReading, wantToRead, read} = this.state;
 
         return(
-            <div className='bookshelf'>
-                <h2 className="bookshelf-title">Currently Reading</h2>
-                <div className='bookshelf-books'>
-                    <ol className='books-grid'>
-                        {currentlyReading.map((book) => (
-                            <li key={book.id}>
-                                <BookCard particularBook={book}/>
-                            </li>
-                        ))}
-                    </ol>
+            <div className="list-books-content">
+                <div className='bookshelf'>
+                    <h2 className="bookshelf-title">Currently Reading</h2>
+                    <div className='bookshelf-books'>
+                        <ol className='books-grid'>
+                            {currentlyReading.map((book) => (
+                                <li key={book.id}>
+                                    <BookCard particularBook={book}/>
+                                </li>
+                            ))}
+                        </ol>
+                    </div>
+                </div>
+                <div className='bookshelf'>
+                    <h2 className="bookshelf-title">Want to Read</h2>
+                    <div className='bookshelf-books'>
+                        <ol className='books-grid'>
+                            {wantToRead.map((book) => (
+                                <li key={book.id}>
+                                    <BookCard particularBook={book}/>
+                                </li>
+                            ))}
+                        </ol>
+                    </div>
+                </div>
+                <div className='bookshelf'>
+                    <h2 className="bookshelf-title">Read</h2>
+                    <div className='bookshelf-books'>
+                        <ol className='books-grid'>
+                            {read.map((book) => (
+                                <li key={book.id}>
+                                    <BookCard particularBook={book}/>
+                                </li>
+                            ))}
+                        </ol>
+                    </div>
                 </div>
             </div>
         )
